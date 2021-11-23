267ff0290278376713f7fd170027f2e9672f6c6c
Van Gastel Robbe <vangastelrobbe@gmail.com>
2021-11-23 01:28:30 +0100

    First components (Learning)
    [Change] tailwind.config.js mode - jit

---
+++ rvg-portfolio/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React, { forwardRef } from 'react'
+
+export default function Button(props)
+{
+    return (
+        <button>
+            {...props}
+        </button>
+    );
+}
+
+export default Button;
--- rvg-portfolio/tailwind.config.js
+++ rvg-portfolio/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit', //just in time mode - faster builds - (dev preview)
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], //removes unused styles
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
