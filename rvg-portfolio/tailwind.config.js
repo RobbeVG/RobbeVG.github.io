e30a238cc2e5e73994ac6f022e2b3b3f7dc71318
Van Gastel Robbe <vangastelrobbe@gmail.com>
2021-11-23 02:28:47 +0100

    Rebase website (React-Vite-Tailwind-Three)
    Rebased the website to use vite and react. (Which already helped a lot with the time it took to set up dev server). Installed tailwind and three library as I am planning to use 3D effects.
    
    I had to change package.json to
    5:    "dev": "CHOKIDAR_USEPOLLING=true vite",
    Because of HMR problems in firefox

--- rvg-portfolio/tailwind.config.js
+++ rvg-portfolio/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit', //just in time mode - faster builds - (dev preview)
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], //removes unused styles
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
